b5d7634c3479ea01591715371b4b83cbeb713d0b
Ritik Anubhav <Ritik.Anubhav@consilio.com>
2024-09-10 12:21:00 +0530

    cors enabled  and jwt code added

--- CMD.Appointment.Domain/CMD.Appointment.ApiService/Program.cs
+++ CMD.Appointment.Domain/CMD.Appointment.ApiService/Program.cs
@@ -1,6 +1,10 @@
 
+using System.Text;
 using CMD.Appointment.Data;
+using CMD.Appointment.Domain.IRepositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CMD.Appointment.ApiService
 {
@@ -21,6 +25,45 @@ namespace CMD.Appointment.ApiService
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            //Adding Jwt Bearer for Verifying Token
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            // Add CORS services
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                    policyBuilder =>
+                    {
+                        policyBuilder.AllowAnyOrigin()
+                                     .AllowAnyMethod()
+                                     .AllowAnyHeader();
+                    });
+            });
+
+            //add depencies to inject
+            builder.Services.AddTransient<AppointmentDbContext>();
+            builder.Services.AddTransient<IAppointmentRepo,AppointmentRepo>();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -31,9 +74,9 @@ namespace CMD.Appointment.ApiService
             }
 
             app.UseHttpsRedirection();
+            app.UseCors("AllowSpecificOrigin");
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
